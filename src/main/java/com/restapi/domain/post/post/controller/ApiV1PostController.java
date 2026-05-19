39b1fad11a99b723e84a0dc9f70ca4ff33bdb70a
kjh3165 <jhwk109@gmail.com>
2026-05-19 10:46:46 +0900

    @RestController == @Controller + @ResponseBody, 클래스 레벨에 @RequestMapping 추가하여 공통 접두어 추가

--- src/main/java/com/restapi/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/restapi/domain/post/post/controller/ApiV1PostController.java
@@ -3,19 +3,19 @@ package com.restapi.domain.post.post.controller;
 import com.restapi.domain.post.post.entity.Post;
 import com.restapi.domain.post.post.service.PostService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController // @Controller + @ResponseBody
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class ApiV1PostController {
     private final PostService postService;
 
-    @GetMapping("/api/v1/posts")
-    @ResponseBody
+    @GetMapping
     public List<Post> getItems() {
         List<Post> items = postService.getList();
 
