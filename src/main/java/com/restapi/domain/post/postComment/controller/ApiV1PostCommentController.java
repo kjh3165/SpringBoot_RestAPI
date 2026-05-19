44e807272e6abb5bc4f319777fddfe726b3e20a4
kjh3165 <jhwk109@gmail.com>
2026-05-19 15:32:14 +0900

    RsData에서 data 필드를 비우고 싶다면 명시적으로 RsData<Void> 사용

--- src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -49,7 +49,7 @@ public class ApiV1PostCommentController {
 
     @Transactional
     @GetMapping("/{id}/delete")
-    public RsData<PostCommentDto> delete(
+    public RsData<Void> delete(
             @PathVariable long postId,
             @PathVariable long id
     ) {
@@ -59,6 +59,6 @@ public class ApiV1PostCommentController {
 
         postService.deleteComment(post, postComment);
 
-        return new RsData<>("200-1","%d번 댓글이 삭제되었습니다.".formatted(id), new PostCommentDto(postComment));
+        return new RsData<>("200-1","%d번 댓글이 삭제되었습니다.".formatted(id));
     }
 }
