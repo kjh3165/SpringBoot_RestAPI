68ada2253e88a4d6e7ce6ddc3b5040b942648074
kjh3165 <jhwk109@gmail.com>
2026-05-20 16:39:52 +0900

    글 작성을 POST로 요청, 요청 본문은 JSON 형태로 기술

--- src/main/java/com/restapi/domain/post/post/controller/ApiV1PostController.java
+++ src/main/java/com/restapi/domain/post/post/controller/ApiV1PostController.java
@@ -4,6 +4,9 @@ import com.restapi.domain.post.post.dto.PostDto;
 import com.restapi.domain.post.post.entity.Post;
 import com.restapi.domain.post.post.service.PostService;
 import com.restapi.global.rsData.RsData;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -40,5 +43,26 @@ public class ApiV1PostController {
         Post post = postService.findById(id);
         postService.delete(post);
         return new RsData<>("200-1", "%d번 게시글이 삭제되었습니다.".formatted(id), new PostDto(post));
+    }
+
+    record PostWriteForm(
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String title,
+            @NotBlank
+            @Size(min = 2, max = 100)
+            String content
+    ){}
+
+    @PostMapping
+    @Transactional
+    public RsData<PostDto> write(@Valid @RequestBody PostWriteForm form) {
+        Post post = postService.create(form.title, form.content);
+
+        return new RsData<>(
+                "200-1",
+                "%d번 게시글이 생성되었습니다.".formatted(post.getId()),
+                new PostDto(post)
+        );
     }
 }
