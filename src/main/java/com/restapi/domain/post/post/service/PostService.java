0016ab2742ea2c7b03b6d91fadfc19e89f27fe61
kjh3165 <jhwk109@gmail.com>
2026-05-22 12:51:49 +0900

    글 작성 테스트 케이스를 조금 더 깐깐하게 수정

--- src/main/java/com/restapi/domain/post/post/service/PostService.java
+++ src/main/java/com/restapi/domain/post/post/service/PostService.java
@@ -7,6 +7,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -58,5 +59,9 @@ public class PostService {
 
     public void delete(Post post) {
         postRepository.delete(post);
+    }
+
+    public Optional<Post> findLatest() {
+        return postRepository.findFirstByOrderByIdDesc();
     }
 }
--- src/test/java/com/restapi/domain/post/post/ApiV1PostControllerTest.java
+++ src/test/java/com/restapi/domain/post/post/ApiV1PostControllerTest.java
@@ -1,5 +1,8 @@
 package com.restapi.domain.post.post;
 
+import com.restapi.domain.post.post.controller.ApiV1PostController;
+import com.restapi.domain.post.post.entity.Post;
+import com.restapi.domain.post.post.service.PostService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test") // 테스트 환경에서는 test 프로파일을 활성화합니다.
 @SpringBootTest // 스프링부트 테스트 클래스임을 나타냅니다.
@@ -23,6 +26,9 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class ApiV1PostControllerTest {
     @Autowired
     private MockMvc mvc; // MockMvc를 주입받습니다.
+
+    @Autowired
+    private PostService postService;
 
     // 글쓰기 테스트
     @Test
@@ -41,9 +47,19 @@ public class ApiV1PostControllerTest {
                                         """)
                 )
                 .andDo(print()); // 응답을 출력합니다.
+
+        Post post = postService.findLatest().get();
+        long totalCount = postService.count();
+
         // 201 Created 상태코드 검증
         resultActions
-                .andExpect(status().isCreated());
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("write"))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.resultCode").value("201-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 게시글이 생성되었습니다.".formatted(post.getId())))
+                .andExpect(jsonPath("$.data.totalCount").value(totalCount))
+                .andExpect(jsonPath("$.data.post.id").value(post.getId()));
     }
 
     //글 수정 테스트
