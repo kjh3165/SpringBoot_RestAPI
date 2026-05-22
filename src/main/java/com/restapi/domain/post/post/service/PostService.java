322176b69f495ab7a7918e3a2e1f98f0e1d074cd
kjh3165 <jhwk109@gmail.com>
2026-05-22 11:16:06 +0900

    댓글 수정 API 구현

--- src/main/java/com/restapi/domain/post/post/service/PostService.java
+++ src/main/java/com/restapi/domain/post/post/service/PostService.java
@@ -51,7 +51,7 @@ public class PostService {
         return post.deleteComment(postComment);
     }
 
-    public void modify(PostComment postComment, String content) {
+    public void modifyComment(PostComment postComment, String content) {
         // 자식 엔티티(PostComment)의 필드 변경 → 더티 체킹에 의해 UPDATE 실행
         postComment.modify(content);
     }
--- src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
+++ src/main/java/com/restapi/domain/post/postComment/controller/ApiV1PostCommentController.java
@@ -3,8 +3,10 @@ package com.restapi.domain.post.postComment.controller;
 import com.restapi.domain.post.post.entity.Post;
 import com.restapi.domain.post.post.service.PostService;
 import com.restapi.domain.post.postComment.dto.PostCommentDto;
+import com.restapi.domain.post.postComment.dto.PostCommentModifyReqBody;
 import com.restapi.domain.post.postComment.entity.PostComment;
 import com.restapi.global.rsData.RsData;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
@@ -57,5 +59,24 @@ public class ApiV1PostCommentController {
         postService.deleteComment(post, postComment);
 
         return new RsData<>("200-1","%d번 댓글이 삭제되었습니다.".formatted(id));
+    }
+
+    @Transactional
+    @PutMapping("/{id}")
+    public RsData<Void> modify(
+            @PathVariable long postId,
+            @PathVariable long id,
+            @Valid @RequestBody PostCommentModifyReqBody reqBody
+    ) {
+        Post post = postService.findById(postId);
+
+        PostComment postComment = post.findCommentById(id).get();
+
+        postService.modifyComment(postComment, reqBody.content());
+
+        return new RsData<>(
+                "200-1",
+                "%d번 댓글이 수정되었습니다.".formatted(id)
+        );
     }
 }
