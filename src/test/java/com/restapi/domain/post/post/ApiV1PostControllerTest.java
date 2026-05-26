7a0a404cd1ebeefc7baf5bf72b52ec270d13b1ea
kjh3165 <jhwk109@gmail.com>
2026-05-26 09:47:51 +0900

    글 삭제 테스트 케이스

--- src/test/java/com/restapi/domain/post/post/ApiV1PostControllerTest.java
+++ src/test/java/com/restapi/domain/post/post/ApiV1PostControllerTest.java
@@ -15,8 +15,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -97,5 +96,26 @@ public class ApiV1PostControllerTest {
 //
 //        assertThat(post.getTitle().equals("제목 update"));
 //        assertThat(post.getContent().equals("내용 update"));
+    }
+
+    @Test
+    @DisplayName("글 삭제")
+    void t3() throws Exception {
+        long id = 1;
+
+        //요청을 보냅니다.
+        ResultActions resultActions = mvc
+                .perform(
+                        delete("/api/v1/posts/" + id)
+                )
+                .andDo(print()); // 응답을 출력합니다.
+
+        // 200 Ok 상태코드 검증
+        resultActions
+                .andExpect(status().isOk())
+                .andExpect(handler().handlerType(ApiV1PostController.class))
+                .andExpect(handler().methodName("delete"))
+                .andExpect(jsonPath("$.resultCode").value("200-1"))
+                .andExpect(jsonPath("$.msg").value("%d번 게시글이 삭제되었습니다.".formatted(id)));
     }
 }
